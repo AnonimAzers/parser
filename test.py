f565ce6bf039658f14358d977984ed663456c572
AnonimAzers <62109114+AnonimAzers@users.noreply.github.com>
2021-09-10 01:30:29 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,20 +1,17 @@
 import requests
 from bs4 import BeautifulSoup as bs
-
-content = requests.get("http://utmiit.ru/ZR/ZR1.htm")
-soup = bs(content.text.encode("cp1252").decode('cp1251'), 'html.parser')
-group_name = input("Введите название группы: ") ##Имя группы
+site = "http://utmiit.ru/ZR/ZR1.htm"
+content = requests.get(site)
+soup = bs(content.text.encode("cp1252").decode("cp1251"), "html.parser")
+group_name = input("Введите название группы: ")
 
 for tr in soup.find_all("tr"):
 	group = tr.find("td", class_="xl20")
 
 	try:
-		if group.text == group_name:
-			print("Группа: {}\n".format(group.text))
-			all_par = tr.find_all("td", class_="xl21")
-			all_cab = tr.find_all("td", class_="xl22")
+		if group_name == group.text:
+			all_info = [x for i, x in enumerate(tr.find_all('td')) if i != 0 and i != 1]
 
-			for i, x in enumerate(all_par):
-				print("{}. {} ({})".format(i+1, x.text.rstrip(), all_cab[i].text))
-	except Exception:
+			print("Все предметы:\n\n{}".format("\n".join([f"{x+1}. {all_info[i].text} ({all_info[i+1].text})" if all_info[i].text != '\xa0' else f"{x+1}. Нету пары ()" for x, i in enumerate(range(0, 8, 2))])))
+	except AttributeError:
 		continue
